7adec0f704b43e413f57fb3751046a64039568c8
yeyderjhjl <jturpoan.@unsa.edu.pe>
2024-06-23 21:40:41 -0500

    actualización de forms.py

--- proyect_gestion/app_software/forms.py
+++ proyect_gestion/app_software/forms.py
@@ -5,3 +5,7 @@ class EstadoRegistroForm(forms.ModelForm):
     class Meta:
         model = EstadoRegistro
         fields = ['estregnom', 'estregestreg']
+        widgets = {
+            'estregnom': forms.TextInput(attrs={'class': 'form-control'}),
+            'estregestreg': forms.TextInput(attrs={'class': 'form-control'}),
+        }
